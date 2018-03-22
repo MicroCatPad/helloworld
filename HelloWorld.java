97fc69b5a4fbc39204eed3e952c69206df1b4636
MicroCatPad <microcatpad@outlook.com>
2018-03-22 18:01:29 +0800

    updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ class HelloWorld {
     static String name = "MicroCatPad";
 
     public static void main(String[] args) {
-        System.out.println(name + " say: Hello World!");
+        System.out.println("Hello "+ name +"!");
     }
 }
